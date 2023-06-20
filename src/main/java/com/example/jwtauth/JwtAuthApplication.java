3afb85b384731abb4e4da4e7b6d9385d0b782db0
secured-jenkins <hasan25.3.2000@hotmail.com>
2023-06-20 11:04:00 +0300

    in microservice context

--- src/main/java/com/example/jwtauth/JwtAuthApplication.java
+++ src/main/java/com/example/jwtauth/JwtAuthApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -18,6 +19,7 @@ import com.example.persistence.entities.Car;
 @ComponentScan({ "com.example.controllers", "com.example.jwt", "com.example.services", "com.example.security", "com.example.configurations"})
 @EntityScan({ "com.example.persistence.entities" })
 @EnableJpaRepositories("com.example.persistence")
+@EnableDiscoveryClient
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class JwtAuthApplication {
 
